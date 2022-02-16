c50a598988f49e69dce5f512745c747a9eb1e246
unknown <kaithwas.697@gmail.com>
2022-02-16 18:58:52 +0530

    Added Multiplication feature; Re-Introduce Addition feature

--- Calculator.sh
+++ Calculator.sh
@@ -1,17 +1,43 @@
-function subtraction() {
-	cal=$(($a-$b))
-	echo "Subtration is $cal"
+# Addition
+function addition() {
+	cal=$(($a+$b))
+	echo "Sum of two numbers is $cal"
 }
+
+# Subtraction
+function subtraction() {
+        cal=$(($a-$b))
+        echo "Subtration is $cal"
+}
+
+# Multiplication
+function multiplication() {
+        cal=$(($a*$b))
+        echo "Multiplication is $cal"
+}
+
 
 echo "Enter a number"
 read a
 echo "Enter another number"
 read b
 
-echo "Enter 1 for subtraction"
+echo "Enter 1 for Addition"
+echo "Enter 2 for Subtraction"
+echo "Enter 3 for Multiplication"
 read c
 
 if [ $c -eq 1 ]
-then subtraction $a $b
-else echo "Invalid operation"
+then
+	addition
+
+elif [ $c -eq 2 ]
+then
+	subtraction
+
+elif [ $c -eq 3 ]
+then
+	multiplication
+else
+	echo "Invalid Operation"
 fi
